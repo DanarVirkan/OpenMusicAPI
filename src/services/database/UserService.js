180246c75878ce12da261586aeeaec4011db216d
DanarVirkan <danarvirkan@gmail.com>
2021-06-19 16:25:45 +0700

    renamed Service files

---
+++ src/services/database/UserService.js
@@ -0,0 +1,37 @@
+const { nanoid } = require("nanoid");
+const { Pool } = require("pg");
+const bcrypt = require('bcrypt');
+const InvariantError = require("../../exceptions/InvariantError");
+
+class UserService {
+    constructor() {
+        this._pool = new Pool();
+    }
+
+    async addUser({ username, password, fullname }) {
+        await this.verifyUser(username);
+        const id = `user-${nanoid(16)}`;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const query = {
+            text: 'INSERT INTO music_user VALUES($1,$2,$3,$4) RETURNING id',
+            values: [id, username, hashedPassword, fullname]
+        };
+        const result = await this._pool.query(query);
+        if (!result.rowCount) {
+            throw new InvariantError('User gagal ditambahkan');
+        }
+        return result.rows[0].id;
+    }
+
+    async verifyUser(username) {
+        const query = {
+            text: 'SELECT username FROM music_user WHERE username = $1',
+            values: [username]
+        };
+        const result = await this._pool.query(query);
+        if (result.rowCount > 0) {
+            throw new InvariantError('Username sudah dipakai');
+        }
+    }
+}
+module.exports = UserService;
