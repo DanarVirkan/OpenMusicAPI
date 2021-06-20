69f939db94b74aa7cf71009b331abd5d97ee28f3
DanarVirkan <danarvirkan@gmail.com>
2021-06-20 20:57:52 +0700

    added catch function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,17 @@
-const mapDBsongsToModel = ({ id, title, performer }) => ({ id, title, performer });
+const ClientError = require('../exceptions/ClientError');
 
-module.exports = { mapDBsongsToModel };
+const mapDBsongsToModel = ({ id, title, performer }) => ({ id, title, performer });
+const catchFunction = (error, h) => {
+  if (error instanceof ClientError) {
+    return h.response({
+      status: 'fail',
+      message: error.message,
+    }).code(error.statusCode);
+  }
+  return h.response({
+    status: 'error',
+    message: 'Maaf, terjadi kegagalan pada server kami.',
+  }).code(500);
+};
+
+module.exports = { mapDBsongsToModel, catchFunction };
