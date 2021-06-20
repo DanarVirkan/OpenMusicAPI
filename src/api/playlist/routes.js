1b200a8332a21c3cc7a8bddd06d3c62eae0c7103
DanarVirkan <danarvirkan@gmail.com>
2021-06-20 17:33:18 +0700

    added auth options in playlist routes

--- src/api/playlist/routes.js
+++ src/api/playlist/routes.js
@@ -2,31 +2,49 @@ const routes = (handler) => [{
   method: 'POST',
   path: '/playlists',
   handler: handler.postPlaylistHandler,
+  options: {
+    auth: 'music_jwt',
+  },
 },
 {
   method: 'GET',
   path: '/playlists',
   handler: handler.getPlaylistHandler,
+  options: {
+    auth: 'music_jwt',
+  },
 },
 {
   method: 'DELETE',
   path: '/playlists/{playlistId}',
   handler: handler.deletePlaylistHandler,
+  options: {
+    auth: 'music_jwt',
+  },
 },
 {
   method: 'POST',
   path: '/playlists/{playlistId}/songs',
   handler: handler.postSongToPlaylistHandler,
+  options: {
+    auth: 'music_jwt',
+  },
 },
 {
   method: 'GET',
   path: '/playlists/{playlistId}/songs',
   handler: handler.getSongInPlaylistHandler,
+  options: {
+    auth: 'music_jwt',
+  },
 },
 {
   method: 'DELETE',
   path: '/playlists/{playlistId}/songs',
   handler: handler.deleteSongInPlaylistHandler,
+  options: {
+    auth: 'music_jwt',
+  },
 },
 ];
 module.exports = routes;
